3a173440e4a6d0ff96b1d29798a35782165d8931
isuc-76/76 <eriasimoyase@gmail.com>
2020-03-07 20:28:44 +0900

    lesson2 finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,28 @@
 class BooksController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def show
-  	@book = Book.find(params[:id])
+  	@showbook = Book.find(params[:id])
+    @book = Book.new
+    @user = @showbook.user
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
+    @user = current_user
   end
 
   def create
+    # p "newbookだよ"
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  		redirect_to book_path(@book), notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
+      @user = current_user
   		@books = Book.all
   		render 'index'
   	end
@@ -33,16 +43,24 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
+      def correct_user
+        @book = Book.find(params[:id])
+        @user = @book.user
+        if current_user != @user
+          redirect_to books_path
+        end
+      end
+
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
